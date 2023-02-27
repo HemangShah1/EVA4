03af60984340a45b5ab672ab072b5a4ed3efbbac
Hemang Shah <hemang@streamoid.com>
2023-02-27 09:59:52 +0530

    minor

--- eva/main.py
+++ eva/main.py
@@ -30,7 +30,7 @@ def create_transforms():
     test_transforms = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])+
+    ])
 
     return train_transforms, test_transforms
 
